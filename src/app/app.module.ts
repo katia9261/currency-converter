f02536ff9ef400880d4e71ea8e474a748e09265a
katia9261 <101433204+katia9261@users.noreply.github.com>
2023-07-17 16:19:16 +0300

    initialized project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './main/header/header.component';
+import { CurrencyConverterComponent } from './main/currency-converter/currency-converter.component';
+import { CurrencyService } from './service/currency.service';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, HeaderComponent, CurrencyConverterComponent],
+  imports: [BrowserModule],
+  providers: [CurrencyService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
