2ea0b0596adab6ea3b2680f43eabdcda5f395539
katia9261 <101433204+katia9261@users.noreply.github.com>
2023-07-18 10:32:50 +0300

    added unit tests

--- src/app/main/currency-converter/currency-converter.component.spec.ts
+++ src/app/main/currency-converter/currency-converter.component.spec.ts
@@ -1,25 +1,87 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatSelectModule } from '@angular/material/select';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CurrencyConverterComponent } from './currency-converter.component';
+import { CurrencyService } from 'src/app/service/currency.service';
+import { of } from 'rxjs';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 describe('CurrencyConverterComponent', () => {
   let component: CurrencyConverterComponent;
   let fixture: ComponentFixture<CurrencyConverterComponent>;
+  let currencyService: CurrencyService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CurrencyConverterComponent ]
-    })
-    .compileComponents();
+      declarations: [CurrencyConverterComponent],
+      providers: [CurrencyService],
+      imports: [
+        ReactiveFormsModule,
+        HttpClientModule,
+        MatSelectModule,
+        BrowserAnimationsModule,
+      ],
+    }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(CurrencyConverterComponent);
     component = fixture.componentInstance;
+    currencyService = TestBed.inject(CurrencyService);
+
+    spyOn(currencyService, 'getConvertedAmount').and.returnValue(
+      of({ result: 100 })
+    );
+
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should initialize conversionForm with default values', () => {
+    expect(component.conversionForm.value).toEqual({
+      currencyFrom: 'UAH',
+      currencyTo: 'EUR',
+      fromAmount: '',
+      toAmount: '',
+    });
+  });
+
+  it('should update conversionForm values when currencyFrom or currencyTo changes', () => {
+    component.currencyFrom.setValue('USD');
+    component.currencyTo.setValue('GBP');
+
+    expect(component.conversionForm.value).toEqual({
+      currencyFrom: 'USD',
+      currencyTo: 'GBP',
+      fromAmount: '',
+      toAmount: '',
+    });
+  });
+
+  it('should switch currencies and convert currency', () => {
+    component.currencyFrom.setValue('USD');
+    component.currencyTo.setValue('GBP');
+    component.switchCurrencies();
+
+    expect(component.conversionForm.value).toEqual({
+      currencyFrom: 'GBP',
+      currencyTo: 'USD',
+      fromAmount: '',
+      toAmount: '',
+    });
+  });
+
+  it('should return an empty string for unknown error', () => {
+    const controlName = 'toAmount';
+    const control = new FormControl('');
+    component.conversionForm = new FormGroup({ [controlName]: control });
+    const errorMessage = component.getErrorMessage(controlName);
+
+    expect(errorMessage).toBe('');
   });
 });
--- src/app/service/currency.service.spec.ts
+++ src/app/service/currency.service.spec.ts
@@ -1,16 +1,71 @@
-import { TestBed } from '@angular/core/testing';
-
+import { TestBed, inject } from '@angular/core/testing';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 import { CurrencyService } from './currency.service';
+import { CurrencySymbol } from '../main/currency.enum';
+import { GetCurrentRates } from '../main/currency.interface';
 
 describe('CurrencyService', () => {
   let service: CurrencyService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [CurrencyService],
+    });
     service = TestBed.inject(CurrencyService);
+    httpMock = TestBed.inject(HttpTestingController);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  afterEach(() => {
+    httpMock.verify();
+  });
+
+  it('should fetch exchange rates successfully', () => {
+    const expectedResponse: GetCurrentRates = {
+      base: CurrencySymbol.USD,
+      rates: {
+        UAH: 27.5,
+      },
+      date: '07-18-2023',
+    };
+
+    service.getExchangeRates(CurrencySymbol.USD).subscribe((response) => {
+      expect(response).toEqual(expectedResponse);
+    });
+
+    const req = httpMock.expectOne(
+      `${(service as any).url}/latest?base=${'USD'}&symbols=${
+        CurrencySymbol.UAH
+      }&places=${(service as any).symbolsAfterComma}`
+    );
+    expect(req.request.method).toBe('GET');
+    req.flush(expectedResponse);
+  });
+
+  it('should convert currency successfully', () => {
+    const from = CurrencySymbol.USD;
+    const to = CurrencySymbol.EUR;
+    const amount = 100;
+    const expectedResponse = {
+      result: 90,
+    };
+
+    service.getConvertedAmount(from, to, amount).subscribe((response) => {
+      expect(response).toEqual(expectedResponse);
+    });
+
+    const req = httpMock.expectOne(
+      `${
+        (service as any).url
+      }/convert?from=${from}&to=${to}&amount=${amount}&places${
+        (service as any).symbolsAfterComma
+      }`
+    );
+    expect(req.request.method).toBe('GET');
+    req.flush(expectedResponse);
   });
 });
