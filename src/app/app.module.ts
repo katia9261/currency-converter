1954043a9b499f69ed5d07c05769826d20bac20a
katia9261 <101433204+katia9261@users.noreply.github.com>
2023-07-18 10:09:47 +0300

    added img

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,21 @@ import { HeaderComponent } from './main/header/header.component';
 import { CurrencyConverterComponent } from './main/currency-converter/currency-converter.component';
 import { CurrencyService } from './service/currency.service';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent, CurrencyConverterComponent],
-  imports: [BrowserModule, HttpClientModule, FormsModule, ReactiveFormsModule],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NoopAnimationsModule,
+    MatSelectModule,
+    MatFormFieldModule,
+  ],
   providers: [CurrencyService],
   bootstrap: [AppComponent],
 })
